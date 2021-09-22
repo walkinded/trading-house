f000a722724e897fb3469dc3d1fc542cae6fba9b
Roman <labunskij1997@gmail.com>
2021-09-22 11:20:58 +0300

    done

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function($) {
+  $('.tab_content').hide();
+  $('.tab_content:first').show();
+  $('.tabs li:first').addClass('active');
+  $('.tabs li').click(function(event) {
+    event.preventDefault();
+    $('.tabs li').removeClass('active');
+    $(this).addClass('active');
+    $('.tab_content').hide();
+
+    var selectTab = $(this).find('a').attr("href");
+
+    $(selectTab).fadeIn();
+  });
+});
